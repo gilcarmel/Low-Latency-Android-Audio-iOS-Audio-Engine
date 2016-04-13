1b14e6250d380033ff8b5b7501561a6f8529d90d
Gil Carmel <gil.carmel@gmail.com>
2016-04-12 21:23:09 -0700

    groping for Android Studio integration with experimental gradle plugin - no dice.

---
+++ Android/MultiMixer/app/src/main/jni/FadeFilter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Gil Carmel on 4/12/16.
+//
+
+#include "FadeFilter.h"
---
+++ Android/MultiMixer/app/src/main/jni/FadeFilter.h
@@ -0,0 +1,14 @@
+//
+// Created by Gil Carmel on 4/12/16.
+//
+
+#ifndef MULTIMIXER_FADEFILTER_H
+#define MULTIMIXER_FADEFILTER_H
+
+
+class FadeFilter {
+
+};
+
+
+#endif //MULTIMIXER_FADEFILTER_H
